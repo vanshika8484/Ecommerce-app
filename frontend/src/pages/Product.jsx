5338feca65498ce799f89f930ab09c5ba140a9ab
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-07-03 21:29:54 +0530

    create product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/frontend_assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
-  return (
-    <div>Product</div>
+  const {productId}=useParams();
+  const {products, currency}=useContext(ShopContext)
+  const [productData, setProductData] = useState(false)
+  const [image,setImage] = useState("")
+  const [size,setSize] = useState("")
+  const fetchProductData=async()=>{
+products.map((product)=>{
+  if(product._id===productId){
+    setProductData(product)
+    setImage(product.image[0])
+    return null;
+  }
+})
+  }
+  useEffect(()=>{
+    fetchProductData()
+  },[productId,products])
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity duration-500 opacity-100 ease-in'>
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+<div className='flex flex-1 flex-col-reverse gap-3 sm:flex-row'>
+  <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+{
+  productData.image.map((img,index)=>{
+    return <img onClick={()=>setImage(img)} src={img} key={index} alt="" className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer'/>
+  })
+}
+  </div>
+  <div className='w-full sm:w-[80%]'>
+     <img src={image} alt="" className='w-full h-auto'/>
+  </div>
+</div>
+{/* PRODUCT INFO */}
+<div className='flex-1'>
+  <h1 className='text-2xl font-medium mt-2'>{productData.name}</h1>
+  <div className='flex items-center gap-1 mt-2'>
+<img src={assets.star_icon} alt="" className='w-3 5' />
+<img src={assets.star_icon} alt="" className='w-3 5' />
+<img src={assets.star_icon} alt="" className='w-3 5' />
+<img src={assets.star_icon} alt="" className='w-3 5' />
+<img src={assets.star_dull_icon} alt="" className='w-3 5' />
+<p className='pl-2'>{122}</p>
+  </div>
+  <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+<p className='mt-5 text-gray-500 w-4/5'>{productData.description}</p>
+<div className='flex flex-col gap-4 my-8'>
+<p>Select Size</p>
+<div className='flex gap-2'>
+{productData.sizes.map((item,index)=>{
+  return <button onClick={()=>setSize(item)} className={`border border-gray-100 py-2 px-4 bg-gray-100 ${item===size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+})}
+</div>
+</div>
+<button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+<hr className='mt-8 sm:w-4/5'/>
+<div className='text-gray-500 mt-5 flex flex-col gap-1 text-sm'>
+<p>100% Original product</p>
+<p>Cash on delivery available</p>
+<p>Easy return and exchange policy within 7 days</p></div>
+</div>
+      </div>
+      <div className='mt-20 '>
+        <div className='flex '>
+          <b className='px-5 py-3 border border-gray-400 text-sm'>Description</b>
+          <p className='px-5 py-3 border border-gray-400 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+<p>An ecommerce is an online platform or business that facilitates the buying and selling of goods or services over the internet. It allows customers to browse, select, and purchase products or services from the comfort of their homes or devices, using secure payment methods and often offering features like product reviews, wishlists, and personalized recommendations.</p>
+      <p>Am ecommerce website typically includes features like product catalogs, shopping carts, secure payment processing, order tracking, and customer support to ensure a seamless and secure shopping experience.</p>
+        </div>
+      </div>
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
+    </div>
+  ) : (
+    <div className='opacity-0'></div>
   )
 }
 
